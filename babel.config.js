d28be09287eeeafe742a97dbf996e7a4708589fd
Egor Zaitsev <zaytsevev@profi.ru>
2019-11-27 16:51:35 +0300

    build script

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const context = path.resolve(__dirname, 'src');
+
 module.exports = {
   presets: [['@babel/preset-typescript', {isTSX: true, allExtensions: true}]],
   plugins: [
+    '@babel/transform-react-jsx',
     [
       'react-css-modules',
       {
         context,
-        generateScopedName: '[name]__[local]',
-        webpackHotModuleReloading: true,
-        handleMissingStyleName: 'ignore',
         autoResolveMultipleImports: true,
+        generateScopedName: '[name]__[local]-[hash:base64:5]',
+        handleMissingStyleName: 'warn',
+        webpackHotModuleReloading: true,
       },
     ],
   ],
---
+++ src/components/block/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default class ProfiUI extends React.Component {
+  render() {
+    return <div>Хелл2оу, кожанные мешки</div>;
+  }
+}
