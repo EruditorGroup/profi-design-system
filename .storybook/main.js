12c21b1cf378a104ad4a0627670e96d1d7a71e8a
Aleksandr Mischenko <h-alex@bk.ru>
2021-01-29 18:41:00 +0300

    fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,16 @@
+const path = require('path');
+const webpackConfig = require('../webpack.config');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, {configType}) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: webpackConfig.module.rules,
+      },
+    };
+  },
+};
