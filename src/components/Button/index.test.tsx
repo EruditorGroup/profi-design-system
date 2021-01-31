37f9a6e9c4f6f68ba4927ba75c279da40f6eccb6
Aleksandr Mischenko <h-alex@bk.ru>
2021-01-31 19:12:43 +0300

    setup jest & enzyme & babel

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import {mount} from 'enzyme';
 import Button from '.';
 
-test('renders learn react link', () => {
-  const component = mount(<Button>learn react</Button>);
-  const text = component.find(/learn react/i);
-  expect(text).not.toBeNull();
+describe('Button', () => {
+  it('renders childrens', () => {
+    const component = mount(
+      <Button className="testbtn">
+        <h1>heading</h1>
+      </Button>,
+    );
+    const text = component.find('h1');
+    expect(text).not.toBeNull();
+  });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -22,27 +22,6 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-typescript',
-                  {isTSX: true, allExtensions: true},
-                ],
-              ],
-              plugins: [
-                '@babel/transform-react-jsx',
-                [
-                  'react-css-modules',
-                  {
-                    context,
-                    autoResolveMultipleImports: true,
-                    generateScopedName: '[name]__[local]-[hash:base64:5]',
-                    handleMissingStyleName: 'warn',
-                    webpackHotModuleReloading: true,
-                  },
-                ],
-              ],
-            },
           },
         ],
       },
