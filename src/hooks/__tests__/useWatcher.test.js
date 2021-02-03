93410f53d03a0bdda3e6fa55382720557a7fb5b8
Aleksandr Mischenko <h-alex@bk.ru>
2021-02-03 18:57:04 +0300

    feat(components): BodyPortal & hooks

---
+++ src/hooks/__tests__/useWatcher.test.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {useWatcher} from '../useWatcher';
+
+jest.mock('react', () => ({
+  useRef: jest.fn(),
+}));
+jest.unmock('../useWatcher');
+
+it('useWatcher', () => {
+  const deps = [1, 2, 3];
+  const newDeps = [1, 3, 4];
+  const ref = {current: deps};
+  React.useRef.mockImplementation(() => ref);
+  const cb = jest.fn();
+
+  useWatcher(cb, newDeps);
+
+  expect(cb).toBeCalled();
+  expect(ref.current).toEqual(newDeps);
+});
---
+++ src/hooks/useWatcher.ts
@@ -0,0 +1,17 @@
+//@flow
+import * as React from 'react';
+
+export function useWatcher(callback: () => any, deps: readonly any[]) {
+  const prevDepsRef = React.useRef(deps);
+  const prevDeps = prevDepsRef.current;
+
+  if (
+    prevDeps !== deps &&
+    (prevDeps.length !== deps.length ||
+      prevDeps.some((item, index) => !Object.is(item, deps[index])))
+  ) {
+    callback();
+  }
+
+  prevDepsRef.current = deps;
+}
