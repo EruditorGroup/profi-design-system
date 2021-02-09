d89538912b3ec2052cc63ca1f69481e384ef20a2
Aleksandr Mischenko <h-alex@bk.ru>
2021-02-09 18:48:39 +0300

    final radio & checkbox

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,23 +3,45 @@ import classnames from 'classnames';
 
 import styles from './Checkbox.module.scss';
 
-// export interface CheckboxProps {}
+// export interface CheckboxProps {
+//   design?: 'brand';
+// }
 
 const Checkbox: React.ForwardRefExoticComponent<
+  // CheckboxProps &
   React.InputHTMLAttributes<HTMLInputElement> &
     React.RefAttributes<HTMLInputElement>
-> = forwardRef(({children, className, key, style, ...props}, ref) => {
-  return (
-    <label
-      style={style}
-      key={key}
-      className={classnames(styles['label'], className)}
-    >
-      <input ref={ref} className={styles['input']} type="checkbox" {...props} />
-      <i className={styles['checker']}></i>
-      <div className={styles['content']}>{children}</div>
-    </label>
-  );
-});
+> = forwardRef(
+  (
+    {
+      // design='brand',
+      children,
+      className,
+      key,
+      style,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <label
+        style={style}
+        key={key}
+        className={classnames(styles['label'], className)}
+      >
+        <input
+          ref={ref}
+          className={styles['input']}
+          type="checkbox"
+          {...props}
+        />
+        <i
+          className={classnames(styles['checker'], styles[`design-brand`])}
+        ></i>
+        <div className={styles['content']}>{children}</div>
+      </label>
+    );
+  },
+);
 
 export default Checkbox;
--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -3,20 +3,35 @@ import classnames from 'classnames';
 
 import styles from './Radio.module.scss';
 
-// export interface RadioProps {}
+// export interface RadioProps {
+//   design?: 'light' | 'brand';
+// }
 
 const Radio: React.ForwardRefExoticComponent<
   // RadioProps &
   React.InputHTMLAttributes<HTMLInputElement> &
     React.RefAttributes<HTMLInputElement>
-> = forwardRef(({children, className, style, ...props}, ref) => {
-  return (
-    <label style={style} className={classnames(styles['label'], className)}>
-      <input ref={ref} className={styles['input']} type="radio" {...props} />
-      <i className={styles['checker']}></i>
-      <div className={styles['content']}>{children}</div>
-    </label>
-  );
-});
+> = forwardRef(
+  (
+    {
+      // design = 'brand',
+      children,
+      className,
+      style,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <label style={style} className={classnames(styles['label'], className)}>
+        <input ref={ref} className={styles['input']} type="radio" {...props} />
+        <i
+          className={classnames(styles['checker'], styles[`design-brand`])}
+        ></i>
+        <div className={styles['content']}>{children}</div>
+      </label>
+    );
+  },
+);
 
 export default Radio;
