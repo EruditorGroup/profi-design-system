7700949984c1d2ae90881243d0fde8ed9ff7ceb3
Aleksandr Mischenko <h-alex@bk.ru>
2021-02-11 14:48:01 +0300

    dropdown final

--- src/components/Dropdown/components/DropdownItem/index.tsx
+++ src/components/Dropdown/components/DropdownItem/index.tsx
@@ -1,20 +1,33 @@
-import React, {forwardRef} from 'react';
+import React, {forwardRef, useContext} from 'react';
 import classnames from 'classnames';
 import styles from './DropdownItem.module.scss';
-import Link, {LinkProps} from '../../../Link';
+import Link from '../../../../components/Link';
+import {DropdownContext} from '../../index';
 
-export interface DropdownItemProps extends LinkProps {
+export interface DropdownItemProps
+  extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, 'href'> {
   divided?: boolean;
+  disabled?: boolean;
+  to?: string;
 }
 
 const DropdownItem: React.ForwardRefExoticComponent<
   DropdownItemProps & React.RefAttributes<HTMLAnchorElement>
-> = forwardRef(({divided = false, ...props}, ref) => {
+> = forwardRef(({to, disabled = false, divided = false, ...props}, ref) => {
+  const context = useContext(DropdownContext);
   return (
     <Link
-      className={classnames(styles['item'], divided && styles['divided'])}
+      className={classnames(
+        styles['item'],
+        divided && styles['divided'],
+        disabled && styles['disabled'],
+        styles[`design-${context?.design}`],
+      )}
+      to={to}
       ref={ref}
       {...props}
+      color={disabled ? 'dark-grey' : 'black'}
+      disabled={disabled}
     />
   );
 });
