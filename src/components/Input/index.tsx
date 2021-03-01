9b13132a32b0b36b0663105321cfb9f2e0059595
Aleksandr Mischenko <h-alex@bk.ru>
2021-03-01 20:04:34 +0300

    fix inputmask types (flow hardcode)

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,51 @@
 import React, {forwardRef, useCallback} from 'react';
-import type {ForwardRefExoticComponent, RefAttributes} from 'react';
-import InputMask, {Props as InputMaskProps} from 'react-input-mask';
+import type {
+  ForwardRefExoticComponent,
+  RefAttributes,
+  Ref,
+  InputHTMLAttributes,
+} from 'react';
+import InputMask from 'react-input-mask';
 import classnames from 'classnames';
 
 import useFloatLabel from './hooks/useFloatLabel';
 
 import styles from './Input.module.scss';
 
-export interface InputProps {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   withFloatLabel?: boolean;
   placeholder?: string;
   block?: boolean;
 }
 
+export interface InputMaskProps {
+  /**
+   * Mask string. Format characters are:
+   * * `9`: `0-9`
+   * * `a`: `A-Z, a-z`
+   * * `\*`: `A-Z, a-z, 0-9`
+   *
+   * Any character can be escaped with backslash, which usually will appear as double backslash in JS strings.
+   * For example, German phone mask with unremoveable prefix +49 will look like `mask="+4\\9 99 999 99"` or `mask={"+4\\\\9 99 999 99"}`
+   */
+  mask: string | Array<string | RegExp>;
+  /**
+   * Character to cover unfilled editable parts of mask. Default character is "_". If set to null, unfilled parts will be empty, like in ordinary input.
+   */
+  maskPlaceholder?: string | null;
+  /**
+   * Show mask even in empty input without focus.
+   */
+  alwaysShowMask?: boolean;
+  /**
+   * Use inputRef instead of ref if you need input node to manage focus, selection, etc.
+   */
+  inputRef?: Ref<HTMLInputElement>;
+}
+
 const Input: ForwardRefExoticComponent<
-  InputProps & Partial<InputMaskProps> & RefAttributes<HTMLInputElement>
+  InputProps & InputMaskProps & RefAttributes<HTMLInputElement>
 > = forwardRef(
   (
     {
