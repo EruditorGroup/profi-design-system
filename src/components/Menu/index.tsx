737a25ccd9507ed1ba0bc9feda15b80ea9928e38
Aleksandr Mischenko <h-alex@bk.ru>
2021-03-01 11:50:05 +0300

    Bump version 0.0.84

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import React, {forwardRef} from 'react';
 import type {
-  AnchorHTMLAttributes,
+  HTMLAttributes,
   ForwardRefExoticComponent,
   RefAttributes,
 } from 'react';
@@ -8,29 +8,23 @@ import classnames from 'classnames';
 import MenuItem from './components/MenuItem';
 import styles from './Menu.module.scss';
 
-export interface MenuProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
-  to?: string;
+export type MenuProps = HTMLAttributes<HTMLDivElement>;
+
+interface MenuComponent
+  extends ForwardRefExoticComponent<MenuProps & RefAttributes<HTMLDivElement>> {
+  Item: typeof MenuItem;
 }
 
 // additional type cast to flowgen
-const Menu: ForwardRefExoticComponent<
-  MenuProps & RefAttributes<HTMLAnchorElement>
-> & {
-  Item: typeof MenuItem;
-} = forwardRef(({to, className, ...props}, ref) => {
+const Menu = forwardRef(({className, ...props}, ref) => {
   return (
-    <a
+    <div
       className={classnames(styles['menu'], className)}
-      href={to}
       ref={ref}
       {...props}
     />
   );
-}) as ForwardRefExoticComponent<
-  MenuProps & RefAttributes<HTMLAnchorElement>
-> & {
-  Item: typeof MenuItem;
-};
+}) as MenuComponent;
 
 Menu.Item = MenuItem;
 
