9ee20305cec1323bbf9ef20b67decef9738451d4
Aleksandr Mischenko <h-alex@bk.ru>
2021-03-03 17:29:12 +0300

    fix lint

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, {forwardRef, useCallback, useEffect} from 'react';
+import React, {forwardRef, useCallback} from 'react';
 import type {
   ForwardRefExoticComponent,
   RefAttributes,
