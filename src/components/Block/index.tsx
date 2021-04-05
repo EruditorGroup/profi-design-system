1b0c94506c371df368ab2e12d83c7c6d95797351
Aleksandr Mischenko <h-alex@bk.ru>
2021-04-05 15:11:22 +0300

    disable flow compiling

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -1,20 +1,24 @@
 import React, {forwardRef} from 'react';
-import type {HTMLAttributes} from 'react';
+import type {
+  HTMLAttributes,
+  ForwardRefExoticComponent,
+  RefAttributes,
+} from 'react';
 import classnames from 'classnames';
 import styles from './Block.module.scss';
 
 export type BlockProps = HTMLAttributes<HTMLDivElement>;
 
-const Block = forwardRef<HTMLDivElement, BlockProps>(
-  ({className, ...props}, ref) => {
-    return (
-      <div
-        className={classnames(styles['block'], className)}
-        ref={ref}
-        {...props}
-      />
-    );
-  },
-);
+const Block: ForwardRefExoticComponent<
+  BlockProps & RefAttributes<HTMLDivElement>
+> = forwardRef(({className, ...props}, ref) => {
+  return (
+    <div
+      className={classnames(styles['block'], className)}
+      ref={ref}
+      {...props}
+    />
+  );
+});
 
 export default Block;
