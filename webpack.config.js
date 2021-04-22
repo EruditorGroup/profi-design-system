2c2496bc0e3b769816953d5fe132542c1f595d21
Aleksandr Mischenko <h-alex@bk.ru>
2021-04-22 15:28:56 +0300

    fix package name for publish

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const package = require('./package.json');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CSS_MODULE_LOCAL_IDENT_NAME_GENERATOR} = require('./.config');
 
-// resolve imports starts with "@profiru/ui" to package root
-// so babel will add @profiru/ui/{src|dist} prefix to each internal import e.g components/, hooks/
+// resolve imports starts with "@EruditorGroup/profi-design-system" to package root
+// so babel will add @EruditorGroup/profi-design-system/{src|dist} prefix to each internal import e.g components/, hooks/
 // that allows to import local components in builded npm module
 function getResolver() {
   return {
