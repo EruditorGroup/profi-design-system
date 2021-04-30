123c9c9e55f96eda7983b55eccf47919c9d42baf
Дмитрий Киреев <kireevdn@profi.ru>
2021-04-30 04:55:27 +0300

    Допиливание монорепы

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import '../packages/ui/src/styles/theme.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
