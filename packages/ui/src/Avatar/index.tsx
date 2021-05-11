e86351d0be4371d0d34cb3e54766477619ae5e31
Aleksandr Karpenko <KarpenkoAP@eruditor-group.com>
2021-05-11 17:58:58 +0300

    fix: build bundle

--- packages/ui/src/Avatar/index.tsx
+++ packages/ui/src/Avatar/index.tsx
@@ -5,6 +5,7 @@ import styles from './Avatar.module.scss';
 import common from '../styles/common.module.css';
 import {ISize} from 'uitype';
 
+// test
 export interface AvatarProps
   extends Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {
   design?: 'circle' | 'rect';
