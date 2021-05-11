326866ffef71607b13ba9ea0dc4f79d0e3b10fb6
Aleksandr Karpenko <KarpenkoAP@eruditor-group.com>
2021-05-11 16:40:07 +0300

    fix: esm build

--- babel.config.js
+++ babel.config.js
@@ -7,13 +7,33 @@ const transformCssImports = !!process.env.CSS_MODULES;
 
 // Production flag, used in a few plugins/presets
 const isProduction = process.env.NODE_ENV === 'production';
+const isCommonJS = process.env.MODULE_TYPE === 'cjs';
+const isESM = process.env.MODULE_TYPE === 'esm';
+
+// Configure css-modules-transform
+const transformCssModulesConfig = {
+  devMode: !isProduction,
+  keepImport: false,
+  generateScopedName: (localName, filePath) =>
+    CSS_MODULE_LOCAL_IDENT_NAME_GENERATOR(localName, filePath),
+  extensions: ['.css', '.scss'],
+  preprocessCss: path.resolve(__dirname, 'processSass.js'),
+};
+
+if (isESM) {
+  transformCssModulesConfig.keepImport = true;
+  transformCssModulesConfig.importPathFormatter = (path) =>
+    path.replace(/(scss|css)$/, 'build.css');
+  transformCssModulesConfig.extractCss = {
+    dir: './esm/',
+    relativeRoot: './src/',
+    filename: '[path]/[name].build.css',
+  };
+}
 
 module.exports = {
   presets: [
-    process.env.MODULE_TYPE === 'cjs' && [
-      '@babel/preset-env',
-      {targets: {node: 'current'}},
-    ],
+    isCommonJS && ['@babel/preset-env', {targets: {node: 'current'}}],
     [
       '@babel/preset-typescript',
       {allExtensions: true, allowDeclareFields: true, isTSX: true},
@@ -23,21 +43,6 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-optional-chaining',
-    transformCssImports && [
-      'css-modules-transform',
-      {
-        devMode: !isProduction,
-        keepImport: false,
-        generateScopedName: (localName, filePath) =>
-          CSS_MODULE_LOCAL_IDENT_NAME_GENERATOR(localName, filePath),
-        extensions: ['.css', '.scss'],
-        preprocessCss: path.resolve(__dirname, 'processSass.js'),
-        extractCss: {
-          dir: './lib/',
-          relativeRoot: './src/',
-          filename: '[path]/[name].css',
-        },
-      },
-    ],
+    transformCssImports && ['css-modules-transform', transformCssModulesConfig],
   ].filter(Boolean),
 };
