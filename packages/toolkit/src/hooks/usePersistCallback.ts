1b006f70f779d448780d7a970143d05470651876
Aleksandr Mischenko <h-alex@bk.ru>
2021-05-13 14:02:04 +0300

    fix(tools): rm back usePersistCallback

--- packages/toolkit/src/hooks/usePersistCallback.ts
+++ packages/toolkit/src/hooks/usePersistCallback.ts
@@ -1,12 +1,16 @@
 import {useEffect, useCallback, useRef} from 'react';
 import type {DependencyList} from 'react';
 
-export default function usePersistCallback<Callback extends Function>(
-  callback: Callback,
-): Callback {
+export default function usePersistCallback<ARG, RET>(
+  callback: (...args: ARG[]) => RET,
+  deps?: DependencyList,
+): (...args: ARG[]) => RET {
   const callbackRef = useRef(callback);
 
-  callbackRef.current = callback;
+  useEffect(() => {
+    callbackRef.current = callback;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
 
-  return useCallback((...args) => callbackRef.current(args), []);
+  return useCallback((...args) => callbackRef.current(...args), []);
 }
