cebcb3b06847c8c86278d145b62bacd2e639133e
Aleksandr Karpenko <KarpenkoAP@eruditor-group.com>
2021-05-14 18:14:35 +0300

    feat(Dropdown): Добавлен :hover trigger, добавлен DropdownItem

--- packages/ui/src/Dropdown/index.tsx
+++ packages/ui/src/Dropdown/index.tsx
@@ -4,16 +4,22 @@ import React, {
   useMemo,
   useEffect,
   useRef,
+  useCallback,
 } from 'react';
-import type {Dispatch, SetStateAction} from 'react';
+import cx from 'classnames';
+
 import DropdownToggler from './components/DropdownToggler';
 import DropdownPortal from './components/DropdownPortal';
-import cx from 'classnames';
+import DropdownItem from './components/DropdownItem';
+
+import type {Dispatch, SetStateAction} from 'react';
+
 import styles from './Dropdown.module.css';
 
 export interface IDropdownContext {
   isOpened: boolean;
   setOpened: Dispatch<SetStateAction<boolean>>;
+  trigger: 'click' | 'hover';
 }
 
 export const DropdownContext = createContext<IDropdownContext | null>(null);
@@ -21,22 +27,41 @@ export const DropdownContext = createContext<IDropdownContext | null>(null);
 export type DropdownProps = {
   className?: string;
   onChange?: (opened: boolean) => void;
+  trigger?: 'click' | 'hover';
 };
 
 interface DropdownComponent extends React.FC<DropdownProps> {
   Toggler: typeof DropdownToggler;
   Portal: typeof DropdownPortal;
+  Item: typeof DropdownItem;
 }
 
-const Dropdown: DropdownComponent = ({children, onChange, className}) => {
+const Dropdown: DropdownComponent = ({
+  children,
+  onChange,
+  className,
+  trigger = 'click',
+}) => {
   const [isOpened, setOpened] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
 
-  const context = useMemo<IDropdownContext>(() => ({isOpened, setOpened}), [
-    isOpened,
-  ]);
+  const context = useMemo<IDropdownContext>(
+    () => ({isOpened, setOpened, trigger}),
+    [isOpened, trigger],
+  );
 
   const onChangeRef = useRef<DropdownProps['onChange']>();
   onChangeRef.current = onChange;
+
+  const onMouseEnter = useCallback(() => {
+    if (trigger !== 'hover') return;
+    setOpened(true);
+  }, [trigger]);
+
+  const onMouseLeave = useCallback(() => {
+    if (trigger !== 'hover') return;
+    setOpened(false);
+  }, [trigger]);
 
   useEffect(() => {
     if (onChangeRef.current) onChangeRef.current(isOpened);
@@ -44,12 +69,20 @@ const Dropdown: DropdownComponent = ({children, onChange, className}) => {
 
   return (
     <DropdownContext.Provider value={context}>
-      <div className={cx(className, styles['relative'])}>{children}</div>
+      <div
+        className={cx(className, styles['relative'])}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        ref={dropdownRef}
+      >
+        {children}
+      </div>
     </DropdownContext.Provider>
   );
 };
 
 Dropdown.Toggler = DropdownToggler;
 Dropdown.Portal = DropdownPortal;
+Dropdown.Item = DropdownItem;
 
 export default Dropdown;
