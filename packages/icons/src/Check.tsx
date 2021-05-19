919b89c4c0ba8820027bbe9150864b6af3674cd5
Дмитрий Киреев <kireevdn@profi.ru>
2021-05-19 13:49:25 +0300

    fix(CheckIcon): Adjust path and viewbox

--- packages/icons/src/Check.tsx
+++ packages/icons/src/Check.tsx
@@ -8,14 +8,14 @@ const CheckIcon: ForwardRefExoticComponent<IconPropsType> = forwardRef(
       <svg
         width="1em"
         height="1em"
-        viewBox="0 0 18 17"
+        viewBox="0 0 17 17"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         {...props}
         ref={ref}
       >
         <path
-          d="M3.25 8.25L7.45 12.25L14.75 3.25"
+          d="M2.5 8.25L6.7 12.25L14 3.25"
           stroke="currentColor"
           stroke-width="2"
           stroke-linecap="round"
