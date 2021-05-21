6380600c0d64b13c2b6ecd701e23735b25be7ca9
Roman Zhigalov <larkie.wistful+github@gmail.com>
2021-05-21 23:34:06 +0300

    feat(ui): Textarea: add autosize feature

--- packages/ui/src/Form/BareTextarea/index.tsx
+++ packages/ui/src/Form/BareTextarea/index.tsx
@@ -1,27 +1,49 @@
-import React, {forwardRef} from 'react';
-import type {TextareaHTMLAttributes} from 'react';
+import React from 'react';
+import TextareaAutosize, {TextareaAutosizeProps} from 'react-textarea-autosize';
 import classnames from 'classnames';
 
 import styles from './BareTextarea.module.scss';
 
 import type {BaseControlProps} from '../types';
 
-export type BareTextareaProps = Omit<
-  TextareaHTMLAttributes<HTMLTextAreaElement>,
-  'size'
-> &
+const DEFAULT_TEXTAREA_ROWS = 3;
+
+export type BareTextareaProps = Omit<TextareaAutosizeProps, 'size'> &
   BaseControlProps<HTMLTextAreaElement> & {
     children?: never;
   };
 
-const BareTextarea = forwardRef<HTMLTextAreaElement, BareTextareaProps>(
-  ({className, inputRef, ...props}) => (
-    <textarea
+const BareTextarea: React.FC<BareTextareaProps> = ({
+  className,
+  rows,
+  minRows,
+  maxRows,
+  inputRef,
+  ...props
+}) => {
+  if (rows !== undefined) {
+    minRows = rows;
+    maxRows = rows;
+  } else if (minRows === undefined && maxRows === undefined) {
+    minRows = DEFAULT_TEXTAREA_ROWS;
+    maxRows = DEFAULT_TEXTAREA_ROWS;
+  }
+
+  return (
+    <TextareaAutosize
       {...props}
+      minRows={minRows}
+      maxRows={maxRows}
       className={classnames(styles['textarea'], className)}
-      ref={inputRef}
-    ></textarea>
-  ),
-);
+      ref={(el) => {
+        if (typeof inputRef === 'function') {
+          inputRef(el);
+        } else if (typeof inputRef === 'object' && inputRef !== null) {
+          inputRef.current = el;
+        }
+      }}
+    />
+  );
+};
 
 export default BareTextarea;
