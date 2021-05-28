b72468f09269c7967625c3381e6a63cfe0a67341
Roman Zhigalov <larkie.wistful+github@gmail.com>
2021-05-28 14:38:10 +0300

    feat(ui): Notification Badge - add "disabled" state

--- packages/ui/src/Notification/index.tsx
+++ packages/ui/src/Notification/index.tsx
@@ -1,17 +1,27 @@
-import React, {HTMLProps} from 'react';
+import React, {HTMLAttributes} from 'react';
 import classnames from 'classnames';
 
 import styles from './Notification.module.scss';
 
-export type NotificationProps = HTMLProps<HTMLDivElement>;
+export type NotificationProps = HTMLAttributes<HTMLDivElement> & {
+  disabled?: boolean;
+};
 
 const Notification: React.FC<NotificationProps> = ({
   className,
+  disabled,
   children,
   ...htmlProps
 }) => (
-  <div className={classnames(styles['notification'], className)} {...htmlProps}>
-    <span>{children}</span>
+  <div
+    className={classnames(
+      styles['notification'],
+      disabled && styles['notification_disabled'],
+      className,
+    )}
+    {...htmlProps}
+  >
+    {children}
   </div>
 );
 
