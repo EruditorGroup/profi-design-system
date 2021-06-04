52d03d56a97aecef27074141bd34808d9ed72b03
Roman Zhigalov <larkie.wistful+github@gmail.com>
2021-06-05 00:56:12 +0300

    feat(ui): Datepicker - alternate mobile appearance

--- packages/ui/src/Datepicker/index.tsx
+++ packages/ui/src/Datepicker/index.tsx
@@ -1,7 +1,9 @@
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useCallback, useMemo, useRef, useState} from 'react';
 import classnames from 'classnames';
 import {CalendarIcon} from '@eruditorgroup/profi-icons';
+import {getReadableDate, useCurrentScreen} from '@eruditorgroup/profi-toolkit';
 
+import BodyPortal from '../BodyPortal';
 import {Calendar} from '../Calendar';
 import Dropdown from '../Dropdown';
 import {Input} from '../Form';
@@ -31,38 +33,79 @@ const Datepicker: React.FC<DatepickerProps> = ({
   onChange: _onChange,
   ...calendarProps
 }) => {
+  const isMobile = useCurrentScreen('mobile', false);
+  const closeDropdownRef = useRef<() => void>();
+
+  const [datepickerOpen, setDatepickerOpen] = useState(false);
   const [selectedDate, setSelectedDate] = useState<Date | null>(
     value ? new Date(value) : null,
   );
-  const label = useMemo(() => selectedDate?.toLocaleString(), [selectedDate]);
+  const label = useMemo(
+    () =>
+      selectedDate
+        ? getReadableDate(selectedDate, {
+            withWeekday: true,
+            withYear: true,
+            omitYearIfThisYear: true,
+          })
+        : '',
+    [selectedDate],
+  );
 
   const onChange: CalendarProps['onChange'] = useCallback(
     ([newDate]: Date[]): void => {
       setSelectedDate(newDate);
       _onChange && _onChange(newDate);
+      closeDropdownRef.current && closeDropdownRef.current();
     },
     [_onChange],
   );
 
+  const calendarComponent = useMemo(
+    () => (
+      <Calendar
+        className={calendarClassName}
+        selectedDays={selectedDate ? [selectedDate] : []}
+        selectionMode="Single"
+        onChange={onChange}
+        {...calendarProps}
+      />
+    ),
+    [selectedDate, calendarClassName, onChange, calendarProps],
+  );
+
   return (
-    <Dropdown data-shmid={shmid} className={className}>
+    <Dropdown
+      data-shmid={shmid}
+      className={className}
+      closeRefHandler={closeDropdownRef}
+      onChange={setDatepickerOpen}
+    >
       <Dropdown.Toggler
         as={Input}
         readOnly
         value={label}
         placeholder={placeholder}
-        className={inputClassName}
+        className={classnames(styles['input'], inputClassName)}
         leading={<CalendarIcon className={styles['icon']} />}
       />
-      <Dropdown.Portal>
-        <Calendar
-          className={classnames(styles['calendar'], calendarClassName)}
-          selectedDays={selectedDate ? [selectedDate] : []}
-          selectionMode="Single"
-          onChange={onChange}
-          {...calendarProps}
-        />
-      </Dropdown.Portal>
+      {isMobile ? (
+        datepickerOpen && (
+          <BodyPortal className={styles['mobile-container']}>
+            <Dropdown.Portal
+              animated={false}
+              position="from-bottom"
+              className={styles['dropdown']}
+            >
+              {calendarComponent}
+            </Dropdown.Portal>
+          </BodyPortal>
+        )
+      ) : (
+        <Dropdown.Portal className={styles['dropdown']}>
+          {calendarComponent}
+        </Dropdown.Portal>
+      )}
     </Dropdown>
   );
 };
