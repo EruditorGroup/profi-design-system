99f34d94f8013644e30037c4f6be126f136ee2de
Roman Zhigalov <larkie.wistful+github@gmail.com>
2021-06-04 13:18:23 +0300

    storybook(ui): Form/preview – actionable icon

--- packages/ui/src/Form/Form.stories.tsx
+++ packages/ui/src/Form/Form.stories.tsx
@@ -8,56 +8,68 @@ export default {
   title: 'Form/Preview',
 } as Meta;
 
-const Template: Story = ({placeholder = 'Label', ...args}) => (
-  <div className="preview">
-    <StoryStyles />
-    <Input
-      {...args}
-      className="preview-item"
-      placeholder="Ваш адрес"
-      trailing={<LocationIcon />}
-    />
-    <Input
-      {...args}
-      className="preview-item preview-item_short"
-      mask="+7 999 999-99-99"
-      placeholder="+7 961 903-00-59"
-      leading={
-        // Russian flag
-        <div
-          style={{
-            height: '14px',
-            width: '17px',
-            marginTop: '5px',
-            borderRadius: '2px',
-            background:
-              'linear-gradient(-180deg, #fff 33.3%, #1653EF 33.3%, #1653EF 66.6%, #EE1B39 66.6%)',
-            transform: 'translateY(-0.5px)',
-          }}
-        />
-      }
-    />
-    <Textarea
-      {...args}
-      minRows={3}
-      className="preview-item"
-      placeholder="Оставьте отзыв..."
-      leading={<LocationIcon />}
-    />
-    <Input
-      {...args}
-      className="preview-item"
-      placeholder="Ваше имя и фамилия"
-    />
-    <Input
-      {...args}
-      className="preview-item"
-      placeholder="Поле заблокировано"
-      disabled
-    />
-    <Input {...args} className="preview-item" placeholder="Эл. почта" invalid />
-  </div>
-);
+const Template: Story = ({placeholder = 'Label', ...args}) => {
+  const onLocationClick: React.MouseEventHandler = (evt) => {
+    // evt.stopPropagation();
+    alert('Сейчас мы определим где ты находишься!');
+  };
+
+  return (
+    <div className="preview">
+      <StoryStyles />
+      <Input
+        {...args}
+        className="preview-item"
+        placeholder="Ваш адрес"
+        trailing={<LocationIcon onClick={onLocationClick} />}
+      />
+      <Input
+        {...args}
+        className="preview-item preview-item_short"
+        mask="+7 999 999-99-99"
+        placeholder="+7 961 903-00-59"
+        leading={
+          // Russian flag
+          <div
+            style={{
+              height: '14px',
+              width: '17px',
+              marginTop: '5px',
+              borderRadius: '2px',
+              background:
+                'linear-gradient(-180deg, #fff 33.3%, #1653EF 33.3%, #1653EF 66.6%, #EE1B39 66.6%)',
+              transform: 'translateY(-0.5px)',
+            }}
+          />
+        }
+      />
+      <Textarea
+        {...args}
+        minRows={3}
+        className="preview-item"
+        placeholder="Оставьте отзыв..."
+        leading={<LocationIcon />}
+      />
+      <Input
+        {...args}
+        className="preview-item"
+        placeholder="Ваше имя и фамилия"
+      />
+      <Input
+        {...args}
+        className="preview-item"
+        placeholder="Поле заблокировано"
+        disabled
+      />
+      <Input
+        {...args}
+        className="preview-item"
+        placeholder="Эл. почта"
+        invalid
+      />
+    </div>
+  );
+};
 export const Preview = Template.bind({});
 
 const StoryStyles = () => (
