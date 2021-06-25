9f417f3b000933569c15a28a425841391df53097
Aleksandr Mischenko <h-alex@bk.ru>
2021-06-25 14:49:17 +0300

    fix(ui): lint

--- packages/ui/src/Space/Space.stories.tsx
+++ packages/ui/src/Space/Space.stories.tsx
@@ -1,17 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Story, Meta} from '@storybook/react';
 
 import Space, {SpaceProps} from './index';
-import Button from '../Button';
 import Text from '../Typography/Text';
 
 export default {
   title: 'Space',
 } as Meta;
-
-const spaceStyles = {
-  margin: '20px',
-};
 
 const Template: Story<SpaceProps> = ({placeholder = 'Label', ...args}) => {
   const variants: SpaceProps[] = [
@@ -19,14 +14,15 @@ const Template: Story<SpaceProps> = ({placeholder = 'Label', ...args}) => {
     {radius: 's', x: 10, y: 15, bg: 'white', withShadow: true},
     {
       align: 'center',
+      justify: 'center',
       bg: 'warning',
       direction: 'row',
       radius: 'm',
       x: 20,
       y: 30,
     },
-    {radius: 'l', x: [15, 5], y: [25, 10], bg: 'light'},
-    {radius: 'xl', x: 40, y: 60, inline: true},
+    {radius: 'l', x: [15, 5], y: [25, 10], bg: 'light', inline: true},
+    {radius: 'xl', x: 40, y: 60},
   ];
   return (
     <div className="preview">
@@ -35,7 +31,7 @@ const Template: Story<SpaceProps> = ({placeholder = 'Label', ...args}) => {
           {(Object.keys(props) as (keyof typeof props)[]).map((key, i, arr) => (
             <Text key={key}>
               {key}=<b>{JSON.stringify(props[key])}</b>
-              {i !== arr.length - 1 && <>,&nbsp;</>}
+              {i !== arr.length - 1 && <>&nbsp;</>}
             </Text>
           ))}
         </Space>
