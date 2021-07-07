bb7638628b99a153dedf5178f074ca10daaff584
Aleksandr Mischenko <h-alex@bk.ru>
2021-07-07 16:23:02 +0300

    test medium

--- storyshots.test.ts
+++ storyshots.test.ts
@@ -44,7 +44,8 @@ const getCustomBrowser = async () => {
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
       '--disable-gpu',
-      '--font-render-hinting=none',
+      '--font-render-hinting=medium',
+      '--enable-font-antialiasing',
     ],
   });
 
