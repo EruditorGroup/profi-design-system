55bd50a0f1bc14e1ff4725e0ee883c33cbd8a474
Aleksandr Mischenko <h-alex@bk.ru>
2021-08-03 14:20:31 +0300

    fix lint

--- packages/toolkit/src/hooks/useDebouncedCallback.ts
+++ packages/toolkit/src/hooks/useDebouncedCallback.ts
@@ -1,9 +1,11 @@
 import {useRef, useCallback} from 'react';
 
 export default function useDebouncedCallback<
+  // осознанный any, не знаем какие аргументы прилетят
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Args extends any[],
   Fn extends (...args: Args) => void
->(callback: Fn, time: number, deps: unknown[]): ReturnType<typeof useCallback> {
+>(callback: Fn, time: number, deps: unknown[]): (...args: Args) => void {
   const timer = useRef<NodeJS.Timeout>();
   const fnRef = useRef(callback);
   fnRef.current = callback;
