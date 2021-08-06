7ce80c0550544a5b9eb4a1ed653e8cf5c005f279
Aleksandr Karpenko <KarpenkoAP@eruditor-group.com>
2021-08-06 15:56:16 +0300

    feat: Добавлена утилита prettyNumber

---
+++ packages/toolkit/src/utils/__tests__/prettyNumber.test.ts
@@ -0,0 +1,10 @@
+import prettyNumber from '../prettyNumber';
+
+jest.unmock('../prettyNumber');
+
+it('prettyNumber', () => {
+  expect(prettyNumber(1000)).toBe('1000');
+  expect(prettyNumber(10000)).toBe('10 000');
+  expect(prettyNumber(10000000)).toBe('10 000 000');
+  expect(prettyNumber(null)).toBe('');
+});
