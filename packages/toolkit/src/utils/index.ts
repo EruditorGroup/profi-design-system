e3e89d9e92a21224c8a2c1428e594034e5a00f4e
Aleksandr Karpenko <KarpenkoAP@eruditor-group.com>
2021-08-06 16:32:01 +0300

    Экспорт утилит

--- packages/toolkit/src/utils/index.ts
+++ packages/toolkit/src/utils/index.ts
@@ -3,3 +3,5 @@ export {default as canUseDom} from './canUseDom';
 export * from './date';
 export {default as stringifyCssProps} from './stringifyCssProps';
 export {default as smoothScroll} from './smoothScroll';
+export {default as plural} from './plural';
+export {default as prettyNumber} from './prettyNumber';
