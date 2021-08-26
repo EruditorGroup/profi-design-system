3b560ca3d904ec7dfecaa576947f8bd37a8aa1c5
Aleksandr Karpenko <KarpenkoAP@eruditor-group.com>
2021-08-26 22:55:59 +0300

    feat: Добавлен дополнительный шрифт для заголовка

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,32 +1,16 @@
-import './fonts/roboto/roboto-v27-latin-400.eot';
+// Roboto
 import './fonts/roboto/roboto-v27-latin-400.woff2';
 import './fonts/roboto/roboto-v27-latin-400.woff';
-import './fonts/roboto/roboto-v27-latin-400.ttf';
-import './fonts/roboto/roboto-v27-cyryllic-400.eot';
-import './fonts/roboto/roboto-v27-cyryllic-400.eot';
 import './fonts/roboto/roboto-v27-cyryllic-400.woff2';
 import './fonts/roboto/roboto-v27-cyryllic-400.woff';
-import './fonts/roboto/roboto-v27-cyryllic-400.ttf';
-import './fonts/roboto/roboto-v27-latin-500.eot';
-import './fonts/roboto/roboto-v27-latin-500.eot';
 import './fonts/roboto/roboto-v27-latin-500.woff2';
 import './fonts/roboto/roboto-v27-latin-500.woff';
-import './fonts/roboto/roboto-v27-latin-500.ttf';
-import './fonts/roboto/roboto-v27-cyryllic-500.eot';
-import './fonts/roboto/roboto-v27-cyryllic-500.eot';
 import './fonts/roboto/roboto-v27-cyryllic-500.woff2';
 import './fonts/roboto/roboto-v27-cyryllic-500.woff';
-import './fonts/roboto/roboto-v27-cyryllic-500.ttf';
-import './fonts/roboto/roboto-v27-latin-700.eot';
-import './fonts/roboto/roboto-v27-latin-700.eot';
 import './fonts/roboto/roboto-v27-latin-700.woff2';
 import './fonts/roboto/roboto-v27-latin-700.woff';
-import './fonts/roboto/roboto-v27-latin-700.ttf';
-import './fonts/roboto/roboto-v27-cyryllic-700.eot';
-import './fonts/roboto/roboto-v27-cyryllic-700.eot';
 import './fonts/roboto/roboto-v27-cyryllic-700.woff2';
 import './fonts/roboto/roboto-v27-cyryllic-700.woff';
-import './fonts/roboto/roboto-v27-cyryllic-700.ttf';
 
 export const parameters = {
   actions: {argTypesRegex: '^on[A-Z].*'},
