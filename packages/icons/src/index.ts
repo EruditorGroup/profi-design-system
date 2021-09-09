bfd2f698cda8c8f5737139077c3f13ffb108df2d
Roman Zhigalov <rzhigalov.dev@gmail.com>
2021-09-09 14:14:24 +0300

    feat(icons): ThumbUp icon

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -43,6 +43,7 @@ export {default as StarIcon} from './Star';
 export {default as StarStrokeIcon} from './StarStroke';
 export {default as SupportIcon} from './Support';
 export {default as SupportThinIcon} from './SupportThin';
+export {default as ThumbUpIcon} from './ThumbUp';
 export {default as VkIcon} from './Vk';
 export {default as YoutubeIcon} from './Youtube';
 export {default as PlaceIcon} from './Place';
