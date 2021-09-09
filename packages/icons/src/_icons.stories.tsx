9616b60f17712f3b16cbea67ac6d5732f0b1fb11
Roman Zhigalov <rzhigalov.dev@gmail.com>
2021-09-09 14:43:29 +0300

    storybook(icons): add search

--- packages/icons/src/_icons.stories.tsx
+++ packages/icons/src/_icons.stories.tsx
@@ -5,7 +5,11 @@ import TableGuides, {TableGuidesProps} from '../../../.storybook/TableGuides';
 
 import * as icons from './index';
 import {IconPropsType} from './_types';
+
+import {Input} from '@eruditorgroup/profi-ui';
+import {SearchIcon} from './index';
 import {IColor} from '@eruditorgroup/profi-toolkit';
+import {useEffect, useState} from '@storybook/addons';
 
 export default {
   title: 'Icons',
@@ -51,21 +55,53 @@ const iconStoryMeta: IconStoryMeta = {
 };
 
 // To add icon to this story you should add export your icon inside './index.tsx'
-export const IconsStory: Story<{color: IColor}> = (args) => (
-  <TableGuides
-    withCellKeyProps
-    cols={iconStoryMeta.cols}
-    rows={iconStoryMeta.rows}
-    Component={({colKey, rowKey, ...props}) => {
-      const Component = (icons as Icons)[colKey ?? ''];
-      return (
-        <div style={{textAlign: 'center'}}>
-          <Component {...args} {...props} />
-        </div>
-      );
-    }}
-  />
-);
+export const IconsStory: Story<{color: IColor}> = (args) => {
+  const [search, setSearch] = useState('');
+  const [meta, setMeta] = useState(iconStoryMeta);
+
+  useEffect(() => {
+    if (!search) {
+      setMeta(iconStoryMeta);
+      return;
+    }
+
+    const searchTerm = search.toLocaleLowerCase();
+    const newMeta = {
+      ...iconStoryMeta,
+      cols: iconStoryMeta.cols.filter(({key}) =>
+        `${key}`.toLocaleLowerCase().includes(searchTerm),
+      ),
+    };
+    setMeta(newMeta);
+  }, [search]);
+
+  return (
+    <div>
+      <Input
+        size="s"
+        leading={<SearchIcon />}
+        value={search}
+        placeholder="Имя иконки"
+        autoComplete="off"
+        onChange={(e) => setSearch(e.target.value)}
+        style={{margin: '20px 0 20px 100px', width: '400px'}}
+      />
+      <TableGuides
+        withCellKeyProps
+        cols={meta.cols}
+        rows={meta.rows}
+        Component={({colKey, rowKey, ...props}) => {
+          const Component = (icons as Icons)[colKey ?? ''];
+          return (
+            <div style={{textAlign: 'center'}}>
+              <Component {...args} {...props} />
+            </div>
+          );
+        }}
+      />
+    </div>
+  );
+};
 
 IconsStory.args = {color: 'primary'};
 IconsStory.storyName = 'Icons';
