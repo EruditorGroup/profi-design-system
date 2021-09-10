c4fcaa54c5ef0090dc06a7e5f1fc6aa2f1e29665
Дмитрий Киреев <kireevdn@profi.ru>
2021-09-10 17:16:04 +0300

    fix(List.Item.MainText): Add className prop

--- packages/ui/src/List/components/Content/MainText.tsx
+++ packages/ui/src/List/components/Content/MainText.tsx
@@ -6,10 +6,15 @@ import {useListContext} from '../..';
 import {useListItemContext} from '../ListItem';
 
 interface MainTextProps {
+  className?: string;
   bold?: boolean;
 }
 
-export const MainText: React.FC<MainTextProps> = ({children, bold}) => {
+export const MainText: React.FC<MainTextProps> = ({
+  className,
+  children,
+  bold,
+}) => {
   const {size, boldItemMainText} = useListContext();
   const disabled = useListItemContext();
   const boldText = bold ?? boldItemMainText;
@@ -17,6 +22,7 @@ export const MainText: React.FC<MainTextProps> = ({children, bold}) => {
   return (
     <div
       className={cx(
+        className,
         styles['main-text'],
         theme.common[`size-${size}`],
         theme.common[`color-${disabled ? 'disabled' : 'secondary'}`],
