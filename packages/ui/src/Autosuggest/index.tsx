10692c90fe4c1de7b6a2144dbbc46dfd669b17c9
Oleg Khateev <okhateeff@gmail.com>
2021-09-16 14:53:26 +0700

    feat(Autosuggest): Добавлены секции в подсказки

--- packages/ui/src/Autosuggest/index.tsx
+++ packages/ui/src/Autosuggest/index.tsx
@@ -1,6 +1,7 @@
 import React, {forwardRef} from 'react';
 import ReactAutosuggest, {
   AutosuggestPropsSingleSection,
+  AutosuggestPropsMultiSection,
 } from 'react-autosuggest';
 import cx from 'classnames';
 import type {AutosuggestPropsBase} from 'react-autosuggest';
@@ -15,6 +16,7 @@ export type ISuggestValue = {
   value: string;
   [key: string]: React.ReactNode;
 };
+
 type RewritedProps =
   | 'ref'
   | 'theme'
@@ -24,19 +26,40 @@ type RewritedProps =
   | 'value'
   | 'onChange';
 
+export type TSection = {suggestions: ISuggestValue[]; title: string};
+
+type SetDifference<A, B> = A extends B ? never : A;
+// eslint-disable-next-line
+type Diff<T extends object, U extends object> = Pick<
+  T,
+  SetDifference<keyof T, keyof U>
+>;
+
+type DiffAutosuggestProps<T extends AutosuggestPropsBase<ISuggestValue>> = Diff<
+  T,
+  AutosuggestPropsBase<ISuggestValue>
+>;
+
+type TMultiVariantProps = DiffAutosuggestProps<
+  Omit<
+    AutosuggestPropsMultiSection<ISuggestValue, TSection>,
+    'getSectionSuggestions'
+  >
+> & {sectionClassName?: string};
+
+type TSingleVariantProps = DiffAutosuggestProps<
+  AutosuggestPropsSingleSection<ISuggestValue>
+>;
+
 export type AutosuggestProps = Omit<
-  AutosuggestPropsBase<ISuggestValue>,
+  AutosuggestPropsBase<ISuggestValue> & InputProps,
   RewritedProps
 > &
-  Omit<
-    AutosuggestPropsSingleSection<ISuggestValue> & InputProps,
-    RewritedProps
-  > & {
+  (TMultiVariantProps | TSingleVariantProps) & {
     suggestionsSize?: ListProps['size'];
-    suggestions: ISuggestValue[];
     isLoading?: boolean;
     isMultiple?: boolean;
-    suggestionsContainerProps: SpaceProps;
+    suggestionsContainerProps?: SpaceProps;
   };
 
 type IAutosuggestComponent = React.ForwardRefExoticComponent<
@@ -51,7 +74,6 @@ const Autosuggest = forwardRef(function Autosuggest(
     size = 'm',
     suggestionsSize = 'm',
     isLoading,
-    suggestions,
     block = false,
     inputClassName,
     containerProps,
@@ -73,75 +95,88 @@ const Autosuggest = forwardRef(function Autosuggest(
   },
   ref,
 ) {
-  return (
-    <ReactAutosuggest<ISuggestValue>
-      ref={ref}
-      theme={styles}
-      suggestions={suggestions}
-      containerProps={{
-        ...containerProps,
-        className: cx(containerProps?.className, styles['root']),
-      }}
-      renderSuggestionsContainer={({containerProps: _props, children, query}) =>
-        query > '' && (
-          <Space
-            withShadow
-            radius={suggestionsSize}
-            bg="white"
-            {..._props}
-            {...(suggestionsContainerProps || {})}
-            className={cx(
-              _props?.className,
-              suggestionsContainerProps?.className,
-              styles['suggestions'],
-              block && styles['block'],
-            )}
+  const baseProps: AutosuggestPropsBase<ISuggestValue> = {
+    ref: ref,
+    theme: styles,
+    containerProps: {
+      ...containerProps,
+      className: cx(containerProps?.className, styles['root']),
+    },
+    renderSuggestionsContainer: ({containerProps: _props, children, query}) =>
+      query > '' && (
+        <Space
+          withShadow
+          radius={suggestionsSize}
+          bg="white"
+          {..._props}
+          {...(suggestionsContainerProps || {})}
+          className={cx(
+            _props?.className,
+            suggestionsContainerProps?.className,
+            styles['suggestions'],
+            block && styles['block'],
+          )}
+        >
+          <List
+            as="div"
+            className={styles['uilist']}
+            design="low"
+            size={suggestionsSize}
           >
-            <List
-              as="div"
-              className={styles['uilist']}
-              design="low"
-              size={suggestionsSize}
-            >
-              {isLoading ? (
-                <Spinner
-                  size={suggestionsSize}
-                  className={styles['spinner']}
-                  color="secondary"
-                />
-              ) : (
-                children
-              )}
-            </List>
-          </Space>
-        )
-      }
-      shouldRenderSuggestions={
-        shouldRenderSuggestions ?? ((value) => value > '')
-      }
-      renderInputComponent={(
-        props: React.InputHTMLAttributes<HTMLInputElement>,
-      ) => (
-        <Input
-          {...props}
-          upper={upper}
-          lower={lower}
-          inputRef={inputRef}
-          className={className}
-          inputClassName={inputClassName}
-          size={size}
-          leading={leading}
-          trailing={trailing}
-          withFloatLabel={withFloatLabel}
-          invalid={invalid}
-          disabled={disabled}
-          style={style}
-        />
-      )}
-      getSuggestionValue={({value}) => value}
-      {...props}
-    />
-  );
+            {isLoading ? (
+              <Spinner
+                size={suggestionsSize}
+                className={styles['spinner']}
+                color="secondary"
+              />
+            ) : (
+              children
+            )}
+          </List>
+        </Space>
+      ),
+    shouldRenderSuggestions: shouldRenderSuggestions ?? ((value) => value > ''),
+    renderInputComponent: (
+      props: React.InputHTMLAttributes<HTMLInputElement>,
+    ) => (
+      <Input
+        {...props}
+        upper={upper}
+        lower={lower}
+        inputRef={inputRef}
+        className={className}
+        inputClassName={inputClassName}
+        size={size}
+        leading={leading}
+        trailing={trailing}
+        withFloatLabel={withFloatLabel}
+        invalid={invalid}
+        disabled={disabled}
+        style={style}
+      />
+    ),
+    getSuggestionValue: ({value}) => value,
+    ...props,
+  };
+  if (props.multiSection === true) {
+    return (
+      <ReactAutosuggest<ISuggestValue, TSection>
+        multiSection={true}
+        suggestions={props.suggestions as TSection[]}
+        getSectionSuggestions={({suggestions}) => suggestions}
+        {...baseProps}
+        theme={{...baseProps.theme, sectionContainer: props.sectionClassName}}
+      />
+    );
+  } else {
+    return (
+      <ReactAutosuggest<ISuggestValue>
+        multiSection={false}
+        suggestions={props.suggestions as ISuggestValue[]}
+        {...baseProps}
+      />
+    );
+  }
 }) as IAutosuggestComponent;
 
 export default Autosuggest;
