0fa8c6b5808704f61570524e37631bec45559fdd
Aleksandr Mischenko <h-alex@bk.ru>
2021-11-19 18:20:24 +0300

    fix stories

--- packages/ui/src/Rate/RateStars/RateStars.stories.tsx
+++ packages/ui/src/Rate/RateStars/RateStars.stories.tsx
@@ -6,7 +6,7 @@ import TableGuides, {
   TableGuidesProps,
 } from '../../../../../.storybook/TableGuides';
 
-const RATE_SIZES: NonNullable<number>[] = [20, 30, 54];
+const RATE_SIZES: number[] = [20, 30, 54];
 
 export default {
   title: 'RateStars',
@@ -34,26 +34,35 @@ const rateStoryMeta: RateStoryMeta = {
 const Template: Story<RateStarsProps & {hovered?: boolean}> = ({
   hovered,
   ...args
-}) => (
-  <TableGuides
-    cols={rateStoryMeta.cols}
-    rows={rateStoryMeta.rows}
-    Component={(props) => {
-      const ref = useRef<HTMLDivElement | null>(null);
-      useLayoutEffect(() => {
-        if (!hovered) return;
-        const event = new MouseEvent('mouseover', {
-          bubbles: true,
-          cancelable: true,
-        });
+}) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  useLayoutEffect(() => {
+    if (!hovered) return;
+    const event = new MouseEvent('mouseover', {
+      bubbles: true,
+      cancelable: true,
+    });
 
-        ref.current.dispatchEvent(event);
-      }, [hovered]);
+    ref.current.dispatchEvent(event);
+  }, [hovered]);
 
-      return <RateStars ref={ref} {...args} {...props} />;
-    }}
-  />
-);
+  return (
+    <div style={{display: 'flex', flexDirection: 'row'}}>
+      {RATE_SIZES.map((size) => (
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          {[...MARKS_ARRAY, '5+'].map((mark) => (
+            <RateStars
+              ref={ref}
+              value={mark}
+              style={{fontSize: `${size}px`}}
+              {...args}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export const Default = Template.bind({});
 Default.storyName = 'Default';
