d8f19c061e8f624210a8b174ac0a73bbb63e00cf
Aleksandr Mischenko <h-alex@bk.ru>
2021-11-29 18:01:45 +0300

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,44 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ["prettier", "plugin:jest/recommended", "plugin:react-hooks/recommended", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
+  extends: [
+    'prettier',
+    'plugin:jest/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'eslint-plugin-babel', 'eslint-plugin-react', 'eslint-plugin-react-hooks', 'jest', 'prettier'],
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-babel',
+    'eslint-plugin-react',
+    'eslint-plugin-react-hooks',
+    'jest',
+    'prettier',
+  ],
   rules: {
     'no-console': 'error',
-    '@typescript-eslint/no-unused-vars': ['warn', {
-      argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_'
-    }]
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
   },
   settings: {
     jest: {
-      version: 26
-    }
-  }
+      version: 26,
+    },
+  },
 };
