f5e4ace792dd81111642706c3890b48e9372ca1a
Oleg Khateev <okhateeff@gmail.com>
2022-02-18 16:11:52 +0700

    fix(Price): Добавил возможность указания диапазона в цене

--- packages/ui/src/Price/index.tsx
+++ packages/ui/src/Price/index.tsx
@@ -7,8 +7,33 @@ import {prettyNumber} from '@eruditorgroup/profi-toolkit';
 
 import type {TextProps} from '../';
 
+type RangeSignature = (
+  from: number,
+  to: number,
+  format: (value: number) => string | number,
+) => React.ReactNode;
+
+const fullRange: RangeSignature = (from, to, format) => (
+  <>
+    {format(from)}
+    {from !== to && (
+      <span>
+        &ndash;
+        {format(to)}
+      </span>
+    )}
+  </>
+);
+
+const partialRange: RangeSignature = (from, to, format) => (
+  <>
+    {(from && 'от ') || (to && 'до ')}
+    {format(from || to)}
+  </>
+);
+
 export interface PriceProps extends TextProps {
-  value: number;
+  value?: number | [number | null, number | null];
   currencyCode: string;
   decimals?: number;
   pretty?: boolean;
@@ -26,15 +51,31 @@ const Price = forwardRef<HTMLSpanElement, PriceProps>(
     },
     ref,
   ) => {
-    if (isNaN(value)) return null;
+    const isRange = typeof value !== 'number';
+    const values = isRange ? value : [value];
+    if (values.every((price) => isNaN(price))) return null;
 
-    const formattedValue = Number.isInteger(value)
-      ? value
-      : value.toFixed(decimals);
+    const [from, to] = values;
+
+    const format = (value: number) => {
+      const formatted = Number.isInteger(value)
+        ? value
+        : value.toFixed(decimals);
+      return pretty ? prettyNumber(formatted) : formatted;
+    };
+
+    let valueView = null;
+    if (from && to) {
+      valueView = fullRange(from, to, format);
+    } else if ((from || to) && isRange) {
+      valueView = partialRange(from, to, format);
+    } else {
+      valueView = format(from);
+    }
 
     return (
       <Text as={Component} ref={ref} {...props}>
-        {pretty ? prettyNumber(formattedValue) : formattedValue}&nbsp;
+        {valueView}&nbsp;
         <CurrencySymbol code={currencyCode} />
       </Text>
     );
