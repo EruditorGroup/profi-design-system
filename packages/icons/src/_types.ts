487d6580edae7805c57bd1f1e5366448e494edc3
Сергей Демченко <rutelun@ya.ru>
2022-03-02 14:28:58 +0300

    Update _types.ts

--- packages/icons/src/_types.ts
+++ packages/icons/src/_types.ts
@@ -1,7 +1,7 @@
 import type {HTMLAttributes, RefAttributes} from 'react';
 import {IColor} from '@eruditorgroup/profi-toolkit';
 
-export interface  IconPropsType
+export interface IconPropsType
   extends HTMLAttributes<SVGSVGElement>,
     RefAttributes<SVGSVGElement> {
   color?: IColor | string;
