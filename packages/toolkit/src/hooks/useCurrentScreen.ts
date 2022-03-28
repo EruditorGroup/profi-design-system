69078d7fb6127dd5ab332086c978daf0ceb9c701
Aleksandr Karpenko <KarpenkoAP@eruditor-group.com>
2022-03-28 17:14:22 +0300

    change useCurrentScreen breakpoint

--- packages/toolkit/src/hooks/useCurrentScreen.ts
+++ packages/toolkit/src/hooks/useCurrentScreen.ts
@@ -3,8 +3,8 @@ import useMedia from './useMedia';
 type ScreenSize = 'mobile' | 'tablet' | 'tabletHorizontal' | 'desktop';
 
 const MEDIA_QUERIES = {
-  mobile: '(min-width: 320px) and (max-width: 731px)',
-  tablet: '(min-width: 732px) and (max-width: 1023px)',
+  mobile: '(min-width: 320px) and (max-width: 767px)',
+  tablet: '(min-width: 768px) and (max-width: 1023px)',
   tabletHorizontal: '(min-width: 1024px) and (max-width: 1280px)',
   desktop: '(min-width: 1280px)',
 };
