ff2ff98f28271675b7b9f0bdfe9ada5d223a14bb
poline <krasnova.poline@gmail.com>
2024-11-06 20:53:13 +0300

    feat: [SEMANTICS-4572] add SortingIcon and GeoIcon

--- packages/icons/src/Sorting.tsx
+++ packages/icons/src/Sorting.tsx
@@ -13,6 +13,7 @@ const SortingIcon: ForwardRefExoticComponent<IconPropsType> = forwardRef(
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         {...combineCommonProps(props)}
+        ref={ref}
       >
         <path d="M1.5 8.5H11.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
         <path d="M1.5 4H15.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
